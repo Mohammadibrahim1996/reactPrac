a533e941a23540138dde704a0650c265e3bbfcea
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-07 23:40:38 +0530

    changes

--- TodoLIst/my-app/src/Components/Counter/Counter.jsx
+++ TodoLIst/my-app/src/Components/Counter/Counter.jsx
@@ -1,32 +1,67 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
 
 export const Counter = () => {
-    const [counter,setCounter]=useState(0);
-    const handleIncreament =()=>{
-        setCounter(counter+1);
-    }
-    const handleDecreament =()=>{
-        setCounter(counter-1);
-    }
-    const handleReset =()=>{
-        setCounter(counter*0);
-    }
+  const [counter, setCounter] = useState(0);
+  const handleIncreament = () => {
+    setCounter(counter + 1 );
+  };
+  const handleDecreament = () => {
+    setCounter(counter - 1);
+  };
+  const handleReset = () => {
+    setCounter(counter * 0);
+  };
 
-
-
-    return (
-<div>
-            <div style={{
-        fontSize: '120%',
-        position: 'relative'}}> 
-                {counter}
-            </div>
-            <div>
-            <button style={{color:"#fff",position:"relative",borderRadius:"5px",backgroundColor: "green"}} onClick={handleIncreament }>Increament</button>
-            <button style={{color:"#fff",position:"relative",borderRadius:"5px",backgroundColor:"red"}} onClick={handleDecreament}>Decrement</button>
-            <button style={{color:"#fff",position:"relative",borderRadius:"5px",backgroundColor:"black"}} onClick={handleReset}>Reset</button>
-            </div>
-  </div>      
-    )
-}
+  return (
+    <div>
+      <div
+        style={{
+          fontSize: "120%",
+          position: "relative",
+        }}
+      >
+        {counter}
+      </div>
+        <button
+          style={{
+            color: "#fff",
+            position: "relative",
+            borderRadius: "5px",
+            backgroundColor: "green",
+            padding: "10px",
+            margin:"10px"
+          }}
+          onClick={handleIncreament}
+        >
+          Increament
+        </button>
+        <button
+          style={{
+            color: "#fff",
+            position: "relative",
+            borderRadius: "5px",
+            backgroundColor: "red",
+            padding: "10px",
+            margin:"10px"
+          }}
+          onClick={handleDecreament}
+        >
+          Decrement
+        </button>
+        <button
+          style={{
+            color: "#fff",
+            position: "relative",
+            borderRadius: "5px",
+            backgroundColor: "black",
+            padding: "10px",
+            margin:"10px"
+          }}
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      
+    </div>
+  );
+};
