b19bca8df267c1ec1e3ad7a9b722d3b05bc80b16
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-07 22:45:14 +0530

    added

--- TodoLIst/my-app/src/App.js
+++ TodoLIst/my-app/src/App.js
@@ -1,14 +1,14 @@
-import {Home} from './Components/Home';
-import {EmpData} from './Components/EmpData/EmpData';
-import {ClassCounter} from "./Components/ClassCounter/Counter";
+// import {Home} from './Components/Home';
+// import {EmpData} from './Components/EmpData/EmpData';
+// import {ClassCounter} from "./Components/ClassCounter/Counter";
+// import './App.css';
 
-import './App.css';
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
-      <EmpData/>
-      <Home/>
+      {/* <ClassCounter/> */}
+      {/* <EmpData/> */}
+      {/* <Home/> */}
         </div>
   );
 }
---
+++ TodoLIst/my-app/src/Components/ToDoListNew/ToDoForm.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function ToDoForm() {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ToDoForm
