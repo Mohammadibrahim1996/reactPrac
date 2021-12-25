c8556e7917d67c554381aefbab6f5a4690ae46b5
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-26 01:11:54 +0530

    added

---
+++ webapp5/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Colg } from "./Colg";
+
+function App() {
+  return (
+    <div className="App">
+      <Colg />
+    </div>
+  );
+}
+
+export default App;
--- webapp5/webapp.js
+++
@@ -1,2 +0,0 @@
-test_data = '[ {country: "Pakistan", score: 23}, {country: "Pakistan", score: 127}, {country: "India", score: 3}, {country: "India", score: 71}, {country: "Australia", score: 31}, {country: "India", score: 22}, {country: "Pakistan", score: 81}, ]';
-server_url = 'http://assessments.reliscore.com/static/misc/cric-scores/';
